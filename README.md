eacec772673477707afbc6db1b752545daa38482
Robinson547 <santiagocompani88@gmail.com>
2025-02-09 22:22:48 +0200

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# Fitness-Recommendation-System
+# Fitness Recommendation System
+
+This is a Python project for fitness recommendation system. More details will be added soon.
---
+++ main.py
@@ -0,0 +1,10 @@
+"""
+Fitness Recommendation System
+This is the main script for the fitness recommendation system project.
+"""
+
+def main():
+    print("Welcome to Fitness Recommendation System!")
+
+if __name__ == "__main__":
+    main()
